e1b2689043539a5f859fd9d0c72905667cab858a
Pavel_Gelfer <9153220501@mail.ru>
2023-11-14 14:00:07 +0300

    1st commit

---
+++ src/main/java/com/ShopCartD14/ShopCartD14/ShopCartD14Application.java
@@ -0,0 +1,13 @@
+package com.ShopCartD14.ShopCartD14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCartD14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopCartD14Application.class, args);
+	}
+
+}
